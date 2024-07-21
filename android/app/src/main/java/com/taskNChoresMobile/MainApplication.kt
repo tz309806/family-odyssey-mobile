f1d3b1b7db3eadfc2ac923ddddad0555301d8e19
Teng Zhang <tz309806@gmail.com>
2024-07-21 10:26:01 -0400

    initial commit

--- android/app/src/main/java/com/taskNChoresMobile/MainApplication.kt
+++ android/app/src/main/java/com/taskNChoresMobile/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.tempproject
+package com.taskNChoresMobile
 
 import android.app.Application
 import com.facebook.react.PackageList
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import 'react-native-gesture-handler'
 import { AppRegistry } from 'react-native';
 import App from './App';  // Adjusted to use App.tsx from the root directory
 import { name as appName } from './app.json';
