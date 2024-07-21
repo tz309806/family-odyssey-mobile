8567755dfba989b5077fd2ca3020ba0d23458dba
Teng Zhang <tz309806@gmail.com>
2024-07-21 11:54:13 -0400

    striped to the bare minimum and now working on android

--- App.tsx
+++ App.tsx
@@ -1,84 +1,19 @@
 import 'react-native-gesture-handler'; // Ensure this is at the top
-import React, { useEffect, useState } from "react";
-import { Image } from 'react-native';
-import { Asset } from 'expo-asset';
-import { Block, GalioProvider } from 'galio-framework';
-import { NavigationContainer } from '@react-navigation/native';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import * as Font from 'expo-font';
-
-import Screens from './src/navigation/Screens';
-import { Images, articles, nowTheme } from './src/constants';
-
-const assetImages = [
-  Images.Onboarding,
-  Images.Logo,
-  Images.Pro,
-  Images.NowLogo,
-  Images.iOSLogo,
-  Images.androidLogo,
-  Images.ProfilePicture,
-  Images.CreativeTimLogo,
-  Images.InvisionLogo,
-  Images.RegisterBackground,
-  Images.ProfileBackground,
-];
-
-articles.forEach(article => assetImages.push(article.image));
-
-function cacheImages(images: any[]) {
-  return images.map(image => {
-    if (typeof image === 'string') {
-      return Image.prefetch(image);
-    } else {
-      return Asset.fromModule(image).downloadAsync();
-    }
-  });
-}
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import { Text, View } from 'react-native';
 
 const App: React.FC = () => {
-  const [isLoadingComplete, setIsLoadingComplete] = useState(false);
-  const [fontLoaded, setFontLoaded] = useState(false);
-
-  useEffect(() => {
-    const loadResourcesAsync = async () => {
-      await Font.loadAsync({
-        'montserrat-regular': require('./src/assets/font/Montserrat-Regular.ttf'),
-        'montserrat-bold': require('./src/assets/font/Montserrat-Bold.ttf'),
-      });
-
-      setFontLoaded(true);
-      await Promise.all([...cacheImages(assetImages)]);
-    };
-
-    loadResourcesAsync().catch(handleLoadingError);
-  }, []);
-
-  const handleLoadingError = (error: any) => {
-    console.warn(error);
-  };
-
-  const handleFinishLoading = () => {
-    if (fontLoaded) {
-      setIsLoadingComplete(true);
-    }
-  };
-
-  if (!isLoadingComplete) {
-    return null;
-  } else {
-    return (
-      <GestureHandlerRootView style={{ flex: 1 }}>
-        <NavigationContainer>
-          <GalioProvider theme={nowTheme}>
-            <Block flex>
-              <Screens />
-            </Block>
-          </GalioProvider>
-        </NavigationContainer>
-      </GestureHandlerRootView>
-    );
-  }
+  return (
+    <GestureHandlerRootView style={{flex: 1}}>
+      <NavigationContainer>
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <Text>Hello world from the file</Text>
+        </View>
+      </NavigationContainer>
+    </GestureHandlerRootView>
+  );
 };
 
 export default App;
--- android/app/src/main/java/com/taskNChoresMobile/MainActivity.kt
+++ android/app/src/main/java/com/taskNChoresMobile/MainActivity.kt
@@ -9,6 +9,10 @@ import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView
 
 class MainActivity : ReactActivity() {
 
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(null)
+//    }
+
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
@@ -20,9 +24,5 @@ class MainActivity : ReactActivity() {
    * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
-          object : DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled) {
-            override fun createRootView(): ReactRootView {
-              return RNGestureHandlerEnabledRootView(this@MainActivity)
-            }
-          }
+          DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
 }
